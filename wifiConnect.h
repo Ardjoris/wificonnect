09081a80cb89a84dadc69bdc44a183c37190b61f
Joris van Velzen <100634009+Ardjoris@users.noreply.github.com>
2022-10-09 08:51:18 +0200

    file to connect wifi

---
+++ wifiConnect.h
@@ -0,0 +1,34 @@
+#include <Settings.h>
+
+// connect wifi
+void WIFIConnect()
+{
+
+    //onboadled aansturing
+pinMode(LED_BUILTIN, OUTPUT);    // Initialize the LED_BUILTIN pin as an output                                   
+digitalWrite(LED_BUILTIN, HIGH); // Ensure LED is off
+
+ipAdressFixed();
+
+    if (WiFi.status() != WL_CONNECTED)
+    { // als wifi niet geconnect
+        // Connect to Wi-Fi network with SSID and password
+        Serial.print("Connecting to ");
+        Serial.println(ssid);
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED)
+        {
+            // blink the LED fast while connecting.
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(250);
+            digitalWrite(LED_BUILTIN, LOW);
+            delay(250);
+            Serial.println("Connecting to WiFi..");
+        }
+        // Print local IP address
+        Serial.println("");
+        Serial.println("WiFi connected.");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+}wifi

2c7be5f146123df15d01ffa77bcfc83ccf758103
Joris van Velzen <100634009+Ardjoris@users.noreply.github.com>
2022-10-09 11:00:03 +0200

    update fixed ipadress

--- settings.h
+++ settings.h
@@ -3,8 +3,9 @@
 
 
 // Fill in Sidd and Password from modem
-const char* ssid     = "sidd";
-const char* password = "passwrd";
+const char* ssid     = "SSID";
+const char* password = "PASSWRD";
+
 
 
 // Set your Static IP address, Gateway, and Subnet)
@@ -14,18 +15,19 @@ IPAddress subnet(255, 255, 255, 0);
 
 
 void ipAdressFixed() {
-    //(un) quote for (un)fixed IP adress
-    if (!WiFi.config(local_IP, gateway, subnet))
-    {
-        Serial.println("STA Failed to configure");
-    }
+    // //(un) quote for (un)fixed IP adress
+    // if (!WiFi.config(local_IP, gateway, subnet))
+    // {
+    //     Serial.println("STA Failed to configure");
+    // } else {Serial.println("It is a FIXED ip adress");}
+
 }
 
-// // MQTT Broker
-// const char* mqtt_broker = "192.168.2.3";
-// const char* topic = "esp001";
-// //const char* topic2= "CTime";
-// const char* topicback = "feedback";
-// const char* mqtt_username = "arduino11";
-// const char* mqtt_password = "wachtw";
-// const int mqtt_port = 1883;
+// MQTT Broker
+const char* mqtt_broker = "192.168.2.3";
+const char* topic = "esp001";
+//const char* topic2= "CTime";
+const char* topicback = "feedback";
+const char* mqtt_username = "arduino11";
+const char* mqtt_password = "wachtw";
+const int mqtt_port = 1883;
